55b4d59ece7bb6da8dab1b89cc39cb8ea81aa650
ashwini kumar <ashwini3326@gmail.com>
2019-11-02 20:39:43 +0530

    apps added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings    #custom_configuration
+from django.conf.urls.static import static  #custom_configuration
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
