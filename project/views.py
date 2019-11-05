a84fd5960876d11b002c40aac20a8dae6f2fddb5
ashwini kumar <ashwini3326@gmail.com>
2019-11-05 19:39:11 +0530

    commit

--- project/views.py
+++ project/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Project
 def index(request):
-    return render(request, 'project/index.html')
+    projects=Project.objects
+    return render(request, 'project/index.html', {"projects":projects})
 
