769ec2394bc91686c2100447e3459b1fb0f9fde5
ashwini kumar <ashwini3326@gmail.com>
2019-11-17 01:06:37 +0530

    bug management system redesigned

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.shortcuts import render
 
 # Create your views here.
 from project.models import Project
+from bug.models import Bug
 def index(request):
     projects=Project.objects.order_by('-datetime')[0:3]
-    return render(request, 'base/index.html', {"projects":projects})
+    bugs=Bug.objects.order_by('-datetime')[0:5]
+    return render(request, 'base/index.html', {"projects":projects,"bugs":bugs,})
 
 
