ad1ba68d66eb66d93689696278ec3c67656ea38e
ashwini kumar <ashwini3326@gmail.com>
2019-12-03 23:57:12 +0530

    added search

--- bug/urls.py
+++ bug/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.bug_index, name='bug_index'),
     path('<int:bug_id>',views.bug_details, name='bug_details'),
+    path('search/', views.bug_search, name='bug_search'),
     
 ]
 
--- bug/views.py
+++ bug/views.py
@@ -9,3 +9,12 @@ def bug_details(request, bug_id):
     bug=get_object_or_404(Bug, pk=bug_id)
     return render(request, 'bug/bug_details.html', {'bug':bug})
 
+from django.db.models import Q
+
+def bug_search(request):
+    search_string = request.GET.get('search_string')
+    object_list = Bug.objects.filter(
+            Q(issue__icontains=search_string) | Q(summary__icontains=search_string)
+            )
+    #object_list= Bug.objects.filter(issue__icontains='n')
+    return render(request, 'bug/bug_search.html', {"bugs":Bug.objects.order_by('-datetime'), "search_string":search_string, "object_list":object_list})
